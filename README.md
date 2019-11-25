235e19ee548084153a3ca96ede90f3c67a4d6cee
Gabriel Perez Paredes <gabogabygpp16@gmail.com>
2019-11-25 14:38:19 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Operadores_Aritmeticos_Combinados_Java
---
+++ src/view/Operaciones.java
@@ -0,0 +1,19 @@
+  package view;
+
+public class Operaciones {
+
+	public static void main(String[] args) {
+		int numero = 10;
+		
+		//numero = numero +5;
+		//linea 8 y linea 10 son lo mismo
+		numero += 5;
+		/* todos los operadores aritmeticos pueden complir esta funcion siempre y 
+		 * cuando esten delante de el =
+		 * (+ - * /)
+		 */
+		
+		System.out.println(numero);
+	}
+
+}
